3246a7647ec9568938140f6a915d769be50bbc07
QUICK APPS STUDIOS <yourGitHub@email.com>
2016-10-03 19:05:23 +0100

    Initial Commit

---
+++ pokedex3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedex3
+//
+//  Created by QUICK APPS STUDIOS on 03/10/2016.
+//  Copyright © 2016 QUICK APPS STUDIOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
